0cbb4ed294f70f73197bf9ff67f85656d95232c2
HenryNguyen0 <heng6208@colorado.edu>
2025-09-09 23:47:22 -0600

    added BGM

--- 2d-project-start/survivors_game.gd
+++ 2d-project-start/survivors_game.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
+@onready var audio = $BGM
 
 func spawn_mob():
 	var new_mob = preload("res://mob.tscn").instantiate()
 	%PathFollow2D.progress_ratio = randf()
 	new_mob.global_position = %PathFollow2D.global_position
 	add_child(new_mob)
+	
+	if !audio.is_playing():
+			audio.play()
 
 
 func _on_timer_timeout():
