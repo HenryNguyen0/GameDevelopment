4802eb5fcf5295105f4ea975573bd5c805750151
HenryNguyen0 <heng6208@colorado.edu>
2025-09-09 22:42:07 -0600

    added hurt sfx

--- 2d-project-start/player.gd
+++ 2d-project-start/player.gd
@@ -2,11 +2,16 @@ extends CharacterBody2D
 
 signal health_depleted
 
+@onready var audio = $"../AudioStreamPlayer"
+
 var health = 100.0
 
+
 func _physics_process(delta):
+	const SPEED = 600.0
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * 600
+	velocity = direction * SPEED
+
 	move_and_slide()
 	
 	if velocity.length() > 0.0:
@@ -14,10 +19,17 @@ func _physics_process(delta):
 	else:
 		%HappyBoo.play_idle_animation()
 	
-	const DAMAGE_RATE = 5.0
-	var overlapping_mobs = %Hurtbox.get_overlapping_bodies()
+	# Taking damage
+	const DAMAGE_RATE = 6.0
+	
+	
+		
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
 	if overlapping_mobs.size() > 0:
 		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
-		%ProgressBar.value = health
+		
+		if !audio.is_playing():
+			audio.play()
+		
 		if health <= 0.0:
 			health_depleted.emit()
